65a618ad3b17139151a7be17a534aff13f96daf2
Ryan Blunden <ryan.blunden@gmail.com>
2023-09-15 13:59:35 +1000

    Fixed failing quality checks

--- chatgpt_clone/utils/storages.py
+++ chatgpt_clone/utils/storages.py
@@ -1,11 +1,13 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
+# pylint: disable=abstract-method,too-few-public-methods
 class StaticRootS3Boto3Storage(S3Boto3Storage):
     location = "static"
     default_acl = "public-read"
 
 
+# pylint: disable=abstract-method,too-few-public-methods
 class MediaRootS3Boto3Storage(S3Boto3Storage):
     location = "media"
     file_overwrite = False
